b06465056381fb8cda66a6bcc76ea046ee0f5520
Wang Kai <wangkai@westlake.edu.cn>
2025-09-23 15:45:45 +0800

    Add yaml_file

--- scripts/SMR.sh
+++ scripts/SMR.sh
@@ -5,13 +5,36 @@
 #  Input
 # ------------------------------------------------------------------------
 CONFIG=$1
-SCRIPT_DIR=`yq .script.path "${CONFIG}"`
-GWAS_DATA=`yq .input.gwas "${CONFIG}"`
-trait_name=`yq .input.trait "${CONFIG}"`
-OUTPUT=`yq .input.output "${CONFIG}"`
+# OUTPUT should be in directory `/data`
 
+SMR=`yq .software.smr "${CONFIG}"`
+trait_name=`yq .input.trait "${CONFIG}"`
+RESULT_OUTPUT=`yq .input.output "${CONFIG}"`
+
+GWAS_DATA=`yq .input.gwas "${CONFIG}"`
+QTL_list=`yq .magic.QTL_list "${CONFIG}"`
+REFERENCE_bld=`yq .reference.reference_bld "${CONFIG}"`
+
+# copy data to the `/data/<job_id>/`
+cache_dir="/data/${SLURM_JOB_ID}"
+mkdir -p ${cache_dir}
+
+time {
+  cp "${GWAS_DATA}"      ${cache_dir}/
+  cp "${QTL_list}"       ${cache_dir}/
+  cp "${REFERENCE_bld}"* ${cache_dir}/
+
+  ls -lih ${cache_dir}/
+}
+
+GWAS_DATA=${cache_dir}/$(basename ${GWAS_DATA})
+REFERENCE_bld=${cache_dir}/$(basename ${REFERENCE_bld})
+QTL_list=${cache_dir}/$(basename ${QTL_list})
+
+OUTPUT=${cache_dir}
 mkdir -p ${OUTPUT}/MAGIC/SMR/detail
 mkdir -p ${OUTPUT}/MAGIC/SMR/summary
+mkdir -p ${RESULT_OUTPUT}
 
 # ------------------------------------------------------------------------
 #  SMR analysis
@@ -49,6 +72,13 @@ if grep -q "AuthenticAMD" /proc/cpuinfo; then
     export MKL_DEBUG_CPU_TYPE=5
 fi
 
+# OpenMP runtime tuning
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_PROC_BIND=close        # keeps neighboring cores (good for shared caches)
+export OMP_PLACES=cores
+export OMP_WAIT_POLICY=active     # can help tight parallel loops (higher power)
+export OMP_DISPLAY_ENV=VERBOSE
+
 "${SMR}" --bld "${REFERENCE_bld}_chr" \
     --gwas-summary "${GWAS_DATA}" \
     --beqtl-list "${QTL_list}" \
@@ -59,5 +89,7 @@ fi
     --out "${OUTPUT}/MAGIC/SMR/detail/${trait_name}"
 
 awk 'NR==1 || FNR>1' ${OUTPUT}/MAGIC/SMR/detail/${trait_name}_${qtl_name}_chr*.msmr > ${OUTPUT}/MAGIC/SMR/summary/${trait_name}_${qtl_name}_chrALL.msmr
+
+cp -r "${OUTPUT}/"* ${RESULT_OUTPUT}/
 
 rm ${OUTPUT}/MAGIC/SMR/detail/${trait_name}_${qtl_name}_chr*
